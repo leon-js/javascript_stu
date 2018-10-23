f64769ce1825b33a1844981cadb4011f6090f06d
lianglihao <807478609@qq.com>
2018-10-23 20:11:54 +0800

    完成恶魔小球游戏

--- object/bouncing balls game/main_finished.js
+++ object/bouncing balls game/main_finished.js
@@ -1,3 +1,8 @@
+// define variable for ball count paragraph
+
+var para = document.querySelector('p');
+var count = 0;
+
 // setup canvas
 
 var canvas = document.querySelector('canvas');
@@ -13,17 +18,21 @@ function random(min,max) {
   return num;
 }
 
-// define Ball constructor
+// define shape constructor
 
 function Shape(x, y, velX, velY, exists) {
   this.x = x;
   this.y = y;
   this.velX = velX;
   this.velY = velY;
+  this.exists = exists;
 }
 
-function Ball(x , y, velX, velY, color, size, exists){
+// define Ball constructor, inheriting from Shape
+
+function Ball(x, y, velX, velY, exists, color, size) {
   Shape.call(this, x, y, velX, velY, exists);
+
   this.color = color;
   this.size = size;
 }
@@ -72,18 +81,105 @@ Ball.prototype.collisionDetect = function() {
       var dy = this.y - balls[j].y;
       var distance = Math.sqrt(dx * dx + dy * dy);
 
-      if (distance < this.size + balls[j].size) {
+      if (distance < this.size + balls[j].size && balls[j].exists) {
         balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
       }
     }
   }
 };
 
+
+
+
+// define EvilCircle constructor, inheriting from Shape
+
+function EvilCircle(x, y, exists) {
+  Shape.call(this, x, y, 20, 20, exists);
+
+  this.color = 'white';
+  this.size = 10;
+}
+
+EvilCircle.prototype = Object.create(Shape.prototype);
+EvilCircle.prototype.constructor = EvilCircle;
+
+
+// define EvilCircle draw method
+
+EvilCircle.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.strokeStyle = this.color;
+  ctx.lineWidth = 3;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+};
+
+
+// define EvilCircle checkBounds method
+
+EvilCircle.prototype.checkBounds = function() {
+  if((this.x + this.size) >= width) {
+    this.x -= this.size;
+  }
+
+  if((this.x - this.size) <= 0) {
+    this.x += this.size;
+  }
+
+  if((this.y + this.size) >= height) {
+    this.y -= this.size;
+  }
+
+  if((this.y - this.size) <= 0) {
+    this.y += this.size;
+  }
+};
+
+// define EvilCircle setControls method
+
+EvilCircle.prototype.setControls = function() {
+  var _this = this;
+  window.onkeydown = function(e) {
+    if(e.keyCode === 65) { // a
+      _this.x -= _this.velX;
+    } else if(e.keyCode === 68) { // d
+      _this.x += _this.velX;
+    } else if(e.keyCode === 87) { // w
+      _this.y -= _this.velY;
+    } else if(e.keyCode === 83) { // s
+      _this.y += _this.velY;
+    }
+  };
+};
+
+// define EvilCircle collision detection
+
+EvilCircle.prototype.collisionDetect = function() {
+  for(var j = 0; j < balls.length; j++) {
+    if( balls[j].exists ) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].exists = false;
+        count--;
+        para.textContent = 'Ball count: ' + count;
+      }
+    }
+  }
+};
+
+
+
 // define array to store balls
 
 var balls = [];
 
 // define loop that keeps drawing the scene constantly
+
+var evil = new EvilCircle(random(0,width), random(0,height), true);
+evil.setControls();
 
 function loop() {
   ctx.fillStyle = 'rgba(0,0,0,0.25)';
@@ -98,18 +194,26 @@ function loop() {
       random(0 + size,height - size),
       random(-7,7),
       random(-7,7),
+      true,
       'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-      size,
-      true
+      size
     );
     balls.push(ball);
+    count++;
+    para.textContent = 'Ball count: ' + count;
   }
 
   for(var i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
+    if(balls[i].exists) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
   }
+
+  evil.draw();
+  evil.checkBounds();
+  evil.collisionDetect();
 
   requestAnimationFrame(loop);
 }
